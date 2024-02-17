31e546785d428f3b304ad1c173d19028997da364
Hussein Nasrallah <109015272+Husein81@users.noreply.github.com>
2024-02-17 22:48:41 +0200

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Routers } from 'react-router-dom' 
 import App from './App.jsx'
 import './index.css'
+import { AuthProvider } from './Context/AuthProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Routers>
+    <AuthProvider>
     <App/>
+    </AuthProvider>
   </Routers>
 )
